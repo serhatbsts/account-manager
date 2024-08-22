3a3d2c3392e5d334eb1dd13b2f955d1678d5c5c9
Serhat Beştaş <serhatbestas9@gmail.com>
2024-08-22 13:52:55 +0300

    pages created links are being edited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Appbar from "./components/Appbar";
+import LoginPage from "./components/LoginPage/LoginPage";
+import HomePage from "./components/HomePage/HomePage";
+import CreateUserPage from "./components/CreateUserPage/CreateUserPage";
+import AccountPage from "./components/AccountPage/AccountPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+         <Router>
+         <Appbar/>
+             <Routes>
+             <Route path="/" element={<HomePage />} />
+             <Route path="/login" element={<LoginPage/>}/>
+             <Route path="/createUser" element={<CreateUserPage/>}/>
+             <Route path="/userPage" element={<AccountPage/>}/>
+             </Routes>
+         </Router>
+     </div>
   );
 }
 
---
+++ src/components/Appbar.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+export default function Appbar() {
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                       BS Account Manager
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
